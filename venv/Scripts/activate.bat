66c067c50bcc64b8d417d52946ed7620487e0719
Jacqueline Navarro <jacqueline.navarro@ufrpe.br>
2024-10-23 11:11:14 -0300

    clonecasa

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\JACQUELINE0490\Documents\SGE\venv
+set VIRTUAL_ENV=C:\Users\Jonathan\Downloads\Programacao_Jac\FAP_Softex_PE\Projeto_SGE\Softex_Flask\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
